5bbd0c44d59980189662f751eb72784966a8a632
Péter Koppány <koppany.peter@autsoft.hu>
2024-06-16 01:15:08 +0200

    Update checkout function to update the status of the rent post type

--- wp-content/plugins/masterlord-solutions/checkout-functions.php
+++ wp-content/plugins/masterlord-solutions/checkout-functions.php
@@ -1,17 +1,16 @@
 <?php
-add_action('woocommerce_order_status_completed', 'change_rent_post_status_on_purchase');
+add_action('woocommerce_thankyou', 'custom_function_for_order_received', 10, 1);
 
-function change_rent_post_status_on_purchase($order_id)
+function custom_function_for_order_received($order_id)
 {
-    // Get the order object
     $order = wc_get_order($order_id);
 
     // Loop through order items
     foreach ($order->get_items() as $item) {
         // Get the product object
-        $product_id = $item->get_product_id();
+        $product = $item->get_product();
         $user_id = get_current_user_id();
-        $rent_post_id = get_rent_post_by_user_id_and_product_id($user_id, $product_id);
+        $rent_post_id = get_rent_post_by_user_id_and_product_id($user_id, $product->get_id());
 
         if ($rent_post_id) {
             update_rent_status($rent_post_id, RENT_STATUS_PURCHASED);
